4cbbb50d98634dffbb4daa015196eff27bbbbbf8
Daniel Mason <danmasson0@gmail.com>
2023-12-08 21:29:22 -0500

    learnt how to use VueDevTools with Pinia, implement async actions with Pinia,matter state and store refs from the state's store.

--- src/App.vue
+++ src/App.vue
@@ -15,23 +15,27 @@
       <button @click="filter = 'favs'">Fav Tasks</button>
     </nav>
     <!-- task list -->
+    <!-- loading -->
+    <span class="loading" v-if="loading">Loading tasks...</span>
     <div class="task-list" v-if="filter === 'all'">
-      <p>You have {{ taskStore.totalCount }} tasks left to do.</p>
-      <div v-for="task in taskStore.tasks">
+      <p>You have {{ totalCount }} tasks left to do.</p>
+      <div v-for="task in tasks">
         <TaskDetails :task="task" />
       </div>
     </div>
     <div class="task-list" v-if="filter === 'favs'">
-      <p>You have {{ taskStore.favCount }} favs left to do.</p>
-      <div v-for="task in taskStore.favs">
+      <p>You have {{ favCount }} favs left to do.</p>
+      <div v-for="task in favs">
         <TaskDetails :task="task" />
       </div>
     </div>
+    <button @click="taskStore.$reset">Reset Tasks</button>
   </main>
 </template>
 
 <script>
 import { ref } from "vue";
+import { storeToRefs } from "pinia";
 import TaskDetails from "./components/TaskDetails.vue"
 import { useTaskStore } from "./stores/TaskStore";
 import TaskForm from "./components/TaskForm.vue";
@@ -40,9 +44,13 @@ export default {
   components: { TaskDetails, TaskForm },
   setup() {
     const taskStore = useTaskStore();
-    const filter = ref("all")
+    const { tasks, isLoading, favs, totalCount, favCount } = storeToRefs(taskStore);
 
-    return { taskStore, filter }
+    // Fetch Tasks
+    taskStore.getTasks();
+    const filter = ref("all");
+
+    return { taskStore, filter, tasks, isLoading, favs, totalCount, favCount }
   }
 }
 </script>
